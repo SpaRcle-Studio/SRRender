0636a9f448564672317cb5b5f5f61d3e0a0c05c3
Monika0000 <rotaru5craft@gmail.com>
2025-01-30 12:11:37 +0100

    (Reflection) Added numeric property drawer.

--- inc/Graphics/UI/UISizeComponent.h
+++ inc/Graphics/UI/UISizeComponent.h
@@ -15,7 +15,7 @@ namespace SR_GRAPH_UI_NS {
     public:
         void Prepare(SR_UTILS_NS::UI::UIModifierContext& context) const override;
 
-        const bool& IsWidthChangeable() const { return m_isWidthChangeable; }
+        SR_NODISCARD const bool& IsWidthChangeable() const { return m_isWidthChangeable; }
         std::vector<float> GetTest() { return m_test; }
 
     protected:
@@ -26,6 +26,14 @@ namespace SR_GRAPH_UI_NS {
         bool m_isWidthChangeable = true;
         /// @property @changeCallback(OnChanged)
         bool m_isHeightChangeable = true;
+        /// @property @changeCallback(OnChanged)
+        int32_t m_width = 100;
+        /// @property @changeCallback(OnChanged) @readOnly
+        int8_t m_int8 = 5;
+        /// @property @changeCallback(OnChanged) @drag(5)
+        uint64_t m_uint64 = 500 + 400;
+        /// @property @changeCallback(OnChanged)
+        float_t m_height = 200.0f;
         /// @property @changeCallback(OnChanged)
         SR_MATH_NS::FSize2 m_size;
         /// @property @changeCallback(OnChanged) @drag(0.01f) @resetValue(1.0f)
