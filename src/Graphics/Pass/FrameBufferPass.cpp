b5288aaa9a60cbaffe494387f738eb30ee06bacb
Monika0000 <rotaru5craft@gmail.com>
2025-07-29 00:20:27 +0100

    (Render) Added missing file.

---
+++ src/Graphics/Pass/FrameBufferPass.cpp
@@ -0,0 +1,63 @@
+//
+// Created by Monika on 22.07.2022.
+//
+
+#include <Graphics/Pass/FrameBufferPass.h>
+#include <Graphics/Types/Framebuffer.h>
+#include <Graphics/Pipeline/Pipeline.h>
+
+#include <Codegen/FrameBufferPass.generated.hpp>
+
+namespace SR_GRAPH_NS {
+    bool FrameBufferPass::Render() {
+        if (m_data.RenderFrameBuffer([this]() { return GroupPass::Render(); })) {
+            return true;
+        }
+        return false;
+    }
+
+    void FrameBufferPass::Update() {
+        m_data.UpdateFrameBuffer([this]() {
+            GroupPass::Update();
+        });
+    }
+
+    void FrameBufferPass::GetFrameBuffers(FrameBuffers& frameBuffers) const {
+        if (auto&& pFrameBuffer = m_data.GetFramebuffer()) {
+            frameBuffers.emplace_back(pFrameBuffer);
+        }
+    }
+
+    const FrameBufferPassData::ClearColors& FrameBufferPass::GetClearColors() const noexcept {
+        return m_data.GetClearColors();
+    }
+
+    void FrameBufferPass::SetRenderTechnique(IRenderTechnique* pRenderTechnique) {
+        m_data.SetRenderTechnique(pRenderTechnique);
+        Super::SetRenderTechnique(pRenderTechnique);
+    }
+
+    /// ----------------------------------------------------------------------------------------------------------------
+
+    bool ClearBuffersPass::Render() {
+        auto&& pFrameBufferPass = dynamic_cast<FrameBufferPass*>(GetParent());
+        if (!pFrameBufferPass) {
+            SR_WARN("ClearBuffersPass::Render() : parent is not FrameBufferPass!");
+            return false;
+        }
+
+        GetPipeline()->EndRender();
+
+        if (m_clearColor) {
+            GetPipeline()->ClearColorBuffer(pFrameBufferPass->GetClearColors());
+        }
+
+        if (m_clearDepth) {
+            GetPipeline()->ClearDepthBuffer(1.f);
+        }
+
+        GetPipeline()->BeginRender();
+
+        return Super::Render();
+    }
+}
